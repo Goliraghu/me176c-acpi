a93b0fe48bbbddabc355afa72588cd538d120273
lambdadroid <lambdadroid@gmail.com>
2019-04-13 13:24:31 +0200

    Android.mk: Build dsdt.aml directly instead of C source file
    The Android port also uses initrd to override the ACPI DSDT table
    now, so generating the C source file is no longer necessary.

--- Android.mk
+++ Android.mk
@@ -3,13 +3,12 @@ LOCAL_PATH := $(call my-dir)
 IASL := $(HOST_OUT_EXECUTABLES)/iasl
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := dsdt.hex
+LOCAL_MODULE := dsdt.aml
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_DSL_FILE := dsdt.dsl
 
 include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/$(LOCAL_DSL_FILE) $(IASL)
-	$(IASL) -p $(basename $@) -tc $<
-	sed -i s/dsdt_aml_code/AmlCode/ $@
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/$(LOCAL_DSL_FILE) | $(IASL)
+	$(IASL) -p $(basename $@) $<
 
 BUILT_ACPI_DSDT_TABLE := $(LOCAL_BUILT_MODULE)
